40706634c18647aced94f555ddc19a2fa869cf59
Oksana Polyakova <ddreamer22011986@gmail.com>
2022-07-13 18:45:08 +0300

    Task#2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
+    
+     import Vimeo from '@vimeo/player';
+     const iframe = document.querySelector('iframe');
+     const player = new Vimeo(iframe);
+     console.log(player);
+   
+    const onPlay = function(data) {
+        const time = data.seconds;
+        localStorage.setItem("videoplayer-current-time", time);
+    };
 
+    let savedTime = localStorage.getItem("videoplayer-current-time");  
+    //  console.log(savedTime);
+
+    player.on('timeupdate', onPlay);
+    player.setCurrentTime(savedTime).then(function(seconds) {
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            break;
+        default:
+            break;
+    }
+});
